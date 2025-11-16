79640bacbfd0fa5151e4d55c9a03983fef37fe9f
zodac <arouge110@msn.com>
2025-11-16 07:22:06 +0000

    [Repo] Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// eslint.config.mjs
+import externalConfig from './code-quality-config/typescript/eslint.config.mjs'
+
+export default [
+    ...externalConfig,
+    // Extend or override rules
+    {
+        rules: {
+
+        }
+    }
+]

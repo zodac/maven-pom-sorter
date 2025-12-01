fb351035caaecd48b2ec01cb052384e27c3c184e
zodac <arouge110@msn.com>
2025-12-01 05:07:15 +0000

    [Test] Fixing integration tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,11 +1,43 @@
 import * as assert from "assert"
 import * as vscode from "vscode"
+import * as path from "path"
+import * as fs from "fs"
 
-suite("Extension Test Suite", () => {
-    vscode.window.showInformationMessage("Start all tests")
+suite("Extension Integration Tests", () => {
+    const testPom = path.join(__dirname, "pom.xml")
 
-    test("Sample test", () => {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5))
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0))
+    setup(() => {
+        if (!fs.existsSync(testPom)) {
+            fs.writeFileSync(testPom, "<project><z/><a/></project>")
+        }
+    })
+
+    teardown(() => {
+        if (fs.existsSync(testPom)) {
+            fs.unlinkSync(testPom)
+        }
+    })
+
+    test("Extension activates", async () => {
+        const ext = vscode.extensions.getExtension("zodac.maven-pom-sorter")
+        assert.ok(ext, "Extension not found")
+        await ext?.activate()
+        assert.strictEqual(ext?.isActive, true)
+    })
+
+    test("Command sorts pom.xml", async () => {
+        const doc = await vscode.workspace.openTextDocument(testPom)
+        const editor = await vscode.window.showTextDocument(doc)
+
+        const unsorted = "<project><z/><a/></project>"
+        await editor.edit(e =>
+            e.replace(new vscode.Range(0, 0, doc.lineCount, 0), unsorted),
+        )
+
+        await vscode.commands.executeCommand("pom-sorter.sortPom")
+        await new Promise(res => setTimeout(res, 300))
+
+        const result = doc.getText()
+        assert.notStrictEqual(result, unsorted, "File should be sorted")
     })
 })
